be447726bab5138661bdd7a202d9bd14e9d94883
being00881234-github <>
2023-06-08 10:41:56 +0530

    bubble sort input wisherí´£

--- bubblesort.py
+++ bubblesort.py
@@ -5,3 +5,13 @@ def bubblesort(list2):
             if list2[j]>list2[j+1]:
                  list2[j],list2[j+1]=list2[j+1],list2[j]
     return list2
+
+
+a=list()
+
+inputs=int(input("No of elments to add:"))
+
+for _ in range(inputs):
+    a.append(int(input()))
+
+bubblesort(a)
